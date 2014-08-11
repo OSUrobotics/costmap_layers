62dc0210ee61773406983e991f7c45a331aee448
Jasper LaFortune <lafortuj@onid.orst.edu>
2014-08-11 14:13:09 -0700

    added ability to update costmap with new paths

--- simple_nav/src/learning/analyze_path.py
+++ simple_nav/src/learning/analyze_path.py
@@ -24,14 +24,41 @@ import argparse
 
 class PathAnalyzer():
 	"""Analyzes a robot's paths and puts them into a costmap-style format"""
-	def __init__(self):
-		rospy.init_node('analyze_path')
+	def __init__(self, parsed_args):
+		package = rospkg.RosPack()
+		package_path = package.get_path('simple_nav')
 
 
-	def load(self, bag_path):
+		if '/' not in parsed_args.paths:
+			self.paths_bag  = package_path + "/bags/paths/" + parsed_args.paths
+		else:
+			self.paths_bag = parsed_args.paths
+		
+
+		bags_folder 			= package_path + '/bags/costmaps/'
+
+		self.new_bag = False
+		if parsed_args.costmap == None:
+			self.new_bag 			= True
+			bag_name				= time.strftime("%Y-%m-%d_%H-%M-%S_costmap.bag")
+		else:
+			if '/' in parsed_args.costmap:
+				bags_folder = ''
+			bag_name 				= parsed_args.costmap
+
+		self.costmap_bag				= bags_folder + bag_name
+
+		print "Analyzing data from paths:"
+		print "\t", self.paths_bag
+		print "into costmap:"
+		print "\t", self.costmap_bag
+
+
+	def _load_paths(self):
 		"""Load saved data from bag file
-		args: bag_path
+		args: None
 		Pre: 
+			- self.paths_bag is full filepath to bagfile
 			- Bag File must exist
 		Post:
 			- self.map_metadata describes map
@@ -39,7 +66,7 @@ class PathAnalyzer():
 			- self.paths is a list containing one PoseArray
 				for each path recorded
 		"""
-		bag 				= rosbag.Bag(bag_path)
+		bag 				= rosbag.Bag(self.paths_bag)
 
 		for topic, msg, t in bag.read_messages(topics=['map_metadata']):
 			self.map_metadata 	= msg
@@ -53,6 +80,26 @@ class PathAnalyzer():
 			self.paths.append(msg)
 
 		bag.close()
+
+		self._msg_to_paths()
+
+
+	def _load_costmap(self):
+		self.cost_grid = np.ones((self.height, self.width))
+
+		if self.new_bag: return
+
+		bag 			= rosbag.Bag(self.costmap_bag)
+
+		for topic, msg, t in bag.read_messages(topics=['costmap']):
+			cost_grid 	= msg
+
+		bag.close()
+
+		height = cost_grid.info.height
+		width = cost_grid.info.width
+		self.cost_grid = np.array(cost_grid.data).reshape((height, width)).astype(np.float)
+		self.cost_grid /= 50
 
 
 	def _msg_to_paths(self):
@@ -73,8 +120,12 @@ class PathAnalyzer():
 		
 
 	def _paths_to_cost(self):
-		self.cost_grid = np.ones((self.height, self.width))
+		
+		print "Analyzing paths..."
+		count = 0
 		for path in self.paths:
+			count += 1
+			print "\t%d of %d" % (count, len(self.paths))
 			distances = np.empty((self.height, self.width))
 			distances.fill(np.inf)
 
@@ -82,14 +133,13 @@ class PathAnalyzer():
 			faraway = 5
 			min_x -= faraway; min_y -= faraway
 			max_x += faraway; max_y += faraway
-			print min_x, min_y, max_x, max_y
+			print "\tPath bounding  box area: %fm by %fm" % ( abs(max_x - min_x), abs(max_y - min_y) )
 			for x in np.arange(min_x, max_x, self.resolution):
 				for y in np.arange(min_y, max_y, self.resolution):
 					i, j = self._world_to_map(x, y)
 					pt = sPoint(x, y)
 					distances[i, j] = path.distance(pt)
 
-			# self._distances_to_cost(distances)
 			self.cost_grid *= np.tanh(distances * (3.0 / faraway))
 
 
@@ -100,7 +150,10 @@ class PathAnalyzer():
 
 
 	def process(self):
-		self._msg_to_paths()
+		self._load_paths()
+		self._load_costmap()
+		# show_image(self.cost_grid)
+
 		self._paths_to_cost()
 
 		# show_image(self.cost_grid)
@@ -111,19 +164,6 @@ class PathAnalyzer():
 		# show_image(self.cost_grid)
 
 
-	def costmap_serve(self):
-		self.costmap = OccupancyGrid()
-		self.costmap.header = self.map_header
-		self.costmap.info = self.map_metadata
-		data = self.cost_grid.flatten()
-		data = list(data)
-		self.costmap.data = data
-		
-		s = rospy.Service('costmap_server', GetMap, self.costmap_request)
-		rospy.loginfo("Service /costmap_serve ready.")
-		rospy.spin()
-
-
 	def save(self):
 		costmap = OccupancyGrid()
 		costmap.header = self.map_header
@@ -132,20 +172,11 @@ class PathAnalyzer():
 		data = list(data)
 		costmap.data = data
 
-		package 				= rospkg.RosPack()
-		bags_folder 			= package.get_path('simple_nav') + '/bags/costmaps/'
-		bag_name				= time.strftime("%Y-%m-%d_%H-%M-%S_costmap.bag")
-		bag 					= rosbag.Bag(bags_folder + bag_name, 'w')
+		bag = rosbag.Bag(self.costmap_bag, 'w')
 		bag.write("costmap", costmap)
 		bag.close()
 
-		info = "Costmap saved to " + bags_folder + bag_name
-		rospy.loginfo(info)
-
-
-	def costmap_request(self, req):
-		rospy.loginfo("Sending Costmap...")
-		return self.costmap
+		print "Costmap saved to " + self.costmap_bag
 
 
 
@@ -158,23 +189,17 @@ def show_image(img):
 
 def parse(argv):
 	parser = argparse.ArgumentParser(description="A node that analyzes a bag file of robot paths into a costmap-readable format.")
-	parser.add_argument('bagfile')
-	parser.add_argument('launch_crap', nargs=argparse.REMAINDER)
+	parser.add_argument('-c', '--costmap')
+	parser.add_argument('paths')
+	# parser.add_argument('launch_crap', nargs=argparse.REMAINDER)
 	parsed_args = parser.parse_args(argv)
 
-	if '/' not in parsed_args.bagfile:
-		package = rospkg.RosPack()
-		bag_path  = package.get_path('simple_nav') + "/bags/paths/" + parsed_args.bagfile
-	else:
-		bag_path = parsed_args.bagfile
-
-	return bag_path
+	return parsed_args
 
 
 def main(argv):
 	
-	p = PathAnalyzer()
-	p.load(parse(argv))
+	p = PathAnalyzer(parse(argv))
 	p.process()
 	p.save()
 
